d36c4d17a5ed0fabe358b6088ef3677316f715cf
Graham Neubig <neubig@gmail.com>
2023-05-23 20:40:42 -0400

    Fix bad import

--- zeno_build/experiments/search_space.py
+++ zeno_build/experiments/search_space.py
@@ -4,9 +4,7 @@ import json
 import os
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Generic, TypeVar
-
-from pyparsing import Any
+from typing import Any, Generic, TypeVar
 
 T = TypeVar("T")
 
