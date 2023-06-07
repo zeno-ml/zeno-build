928ea74cf20f79db905a6b7a84117b7c6c620e40
Graham Neubig <neubig@gmail.com>
2023-06-08 05:14:29 +0900

    Update characters

--- zeno_build/evaluation/text_features/capitalization.py
+++ zeno_build/evaluation/text_features/capitalization.py
@@ -12,7 +12,7 @@ def input_capital_char_ratio(df: DataFrame, ops: ZenoOptions) -> DistillReturn:
         ops: Zeno options
 
     Returns:
-        DistillReturn: Lengths of inputs
+        Ratio of capital characters in input
     """
     return DistillReturn(
         distill_output=df[ops.data_column].str.count(r"[A-Z]")
@@ -21,15 +21,32 @@ def input_capital_char_ratio(df: DataFrame, ops: ZenoOptions) -> DistillReturn:
 
 
 @distill
-def label_capital_char_ratio(df: DataFrame, ops: ZenoOptions) -> DistillReturn:
-    """Ratio of capital letters in the input.
+def output_capital_char_ratio(df: DataFrame, ops: ZenoOptions) -> DistillReturn:
+    """Ratio of capital letters in the output.
 
     Args:
         df: Zeno DataFrame
         ops: Zeno options
 
     Returns:
-        DistillReturn: Lengths of inputs
+        Ratio of capital characters in output
+    """
+    return DistillReturn(
+        distill_output=df[ops.output_column].str.count(r"[A-Z]")
+        / df[ops.output_column].str.len()
+    )
+
+
+@distill
+def label_capital_char_ratio(df: DataFrame, ops: ZenoOptions) -> DistillReturn:
+    """Ratio of capital letters in the labels.
+
+    Args:
+        df: Zeno DataFrame
+        ops: Zeno options
+
+    Returns:
+        Ratio of capital characters in labels
     """
     return DistillReturn(
         distill_output=df[ops.label_column].str.count(r"[A-Z]")
