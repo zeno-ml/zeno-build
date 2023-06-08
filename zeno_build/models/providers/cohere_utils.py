32b25b136484085ae0c9535665954a666e193ef5
zorazrw <zhiruo.zora.wang@gmail.com>
2023-06-08 13:27:42 -0700

    add code generation utils

--- zeno_build/models/providers/cohere_utils.py
+++ zeno_build/models/providers/cohere_utils.py
@@ -8,6 +8,7 @@ from tqdm.asyncio import tqdm_asyncio
 
 from zeno_build.models import global_models, lm_config
 from zeno_build.prompts import chat_prompt
+from zeno_build.prompts.prompt_utils import replace_variables
 
 
 async def _throttled_cohere_acreate(
@@ -84,5 +85,51 @@ async def generate_from_cohere(
             limiter=limiter,
         )
         for full_context in full_contexts
+    ]
+    return await tqdm_asyncio.gather(*async_responses)
+
+
+async def generate_code_from_cohere(
+    variables: list[str],
+    prompt_template: str,
+    model_config: lm_config.LMConfig,
+    temperature: float,
+    max_tokens: int,
+    top_p: float,
+    requests_per_minute: int,
+) -> list[str]:
+    """Generate outputs from the Cohere API.
+
+    COHERE_API_KEY must be set in order for this function to work.
+
+    Args:
+        variables: The variables to be replaced in the prompt template.
+        prompt_template: The prompt template to use.
+        model_config: The model configuration.
+        temperature: The temperature to use.
+        max_tokens: The maximum number of tokens to generate.
+        top_p: The top-p value to use.
+        requests_per_minute: The number of requests per minute to make.
+
+    Returns:
+        The generated outputs.
+    """
+    if "COHERE_API_KEY" not in os.environ:
+        raise ValueError(
+            "COHERE_API_KEY environment variable must be set when using the "
+            "Cohere API."
+        )
+    global_models.cohere_client = cohere.Client(os.environ["COHERE_API_KEY"])
+    limiter = aiolimiter.AsyncLimiter(requests_per_minute)
+    async_responses = [
+        _throttled_cohere_acreate(
+            model=model_config.model,
+            prompt=replace_variables(prompt_template, vars),
+            temperature=temperature,
+            max_tokens=max_tokens,
+            top_p=top_p,
+            limiter=limiter,
+        )
+        for vars in variables
     ]
     return await tqdm_asyncio.gather(*async_responses)
--- zeno_build/models/providers/huggingface_utils.py
+++ zeno_build/models/providers/huggingface_utils.py
@@ -8,6 +8,7 @@ import transformers
 
 from zeno_build.models import lm_config
 from zeno_build.prompts import chat_prompt
+from zeno_build.prompts.prompt_utils import replace_variables
 
 
 def generate_from_huggingface(
@@ -88,6 +89,88 @@ def generate_from_huggingface(
             outputs = model.generate(**encoded_prompts, generation_config=gen_config)
         outputs = outputs[:, encoded_prompts["input_ids"].shape[-1] :]
         results.extend(tokenizer.batch_decode(outputs, skip_special_tokens=True))
+    # Post-processing to get only the system utterance
+    results = [re.split("\n\n", x)[0].strip() for x in results]
+    return results
+
+
+def generate_code_from_huggingface(
+    variables: list[dict[str, str]],
+    prompt_template: str,
+    model_config: lm_config.LMConfig,
+    temperature: float,
+    max_tokens: int,
+    top_p: float,
+) -> list[str]:
+    """Generate outputs from a huggingface model.
+
+    Args:
+        variables: The variables to be replaced in the prompt template.
+        prompt_template: The prompt template to use.
+        model_config: The model configuration.
+        temperature: The temperature to use.
+        max_tokens: The maximum number of tokens to generate.
+        top_p: The top-p value to use.
+
+    Returns:
+        The generated outputs.
+    """
+    # Load model
+    torch_device = "cuda" if torch.cuda.is_available() else "cpu"
+    model_cls = (
+        model_config.model_cls
+        if model_config.model_cls is not None
+        else transformers.AutoModelForCausalLM
+    )
+    tokenizer_cls = (
+        model_config.tokenizer_cls
+        if model_config.tokenizer_cls is not None
+        else transformers.AutoTokenizer
+    )
+    model: transformers.PreTrainedModel = model_cls.from_pretrained(
+        model_config.model,
+        **model_config.model_loader_kwargs,
+    ).to(torch_device)
+    tokenizer: transformers.PreTrainedTokenizer = tokenizer_cls.from_pretrained(
+        model_config.model
+    )
+    tokenizer.padding_side = "left"
+    if not tokenizer.pad_token:
+        tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+
+    gen_config = transformers.GenerationConfig(
+        do_sample=True,
+        temperature=temperature,
+        max_new_tokens=max_tokens,
+        top_p=top_p,
+        eos_token_id=tokenizer.eos_token_id,
+        pad_token_id=tokenizer.pad_token_id,
+    )
+    # Generate
+    filled_prompts = [replace_variables(prompt_template, vars) for vars in variables]
+    # Process in batches
+    results = []
+    batch_size = 8
+    for i in tqdm.trange(0, len(filled_prompts), batch_size):
+        batch = filled_prompts[i : i + batch_size]
+        encoded_prompts = tokenizer(
+            batch,
+            padding=True,
+            truncation=True,
+            max_length=max_tokens,
+            return_tensors="pt",
+        ).to(torch_device)
+        with torch.no_grad():
+            outputs = model.generate(**encoded_prompts, generation_config=gen_config)
+        outputs = outputs[:, encoded_prompts["input_ids"].shape[-1] :]
+        results.extend(
+            tokenizer.batch_decode(
+                outputs,
+                skip_special_tokens=True,
+                truncate_before_pattern=["\nclass", "\ndef", "\n#", "\nif", "\nprint"],
+            )
+        )
     # Post-processing to get only the system utterance
     results = [re.split("\n\n", x)[0].strip() for x in results]
     return results
