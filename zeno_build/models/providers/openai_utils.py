74495c3c881caad5f9f76c9d7b904412c3f10cec
Graham Neubig <neubig@gmail.com>
2023-06-26 18:10:00 -0400

    Some fixes to OpenAI utils

--- zeno_build/models/providers/openai_utils.py
+++ zeno_build/models/providers/openai_utils.py
@@ -38,9 +38,30 @@ async def _throttled_openai_completion_acreate(
                     "OpenAI API rate limit exceeded. Sleeping for 10 seconds."
                 )
                 await asyncio.sleep(10)
+            except asyncio.exceptions.TimeoutError:
+                logging.warning("OpenAI API timeout. Sleeping for 10 seconds.")
+                await asyncio.sleep(10)
+            except openai.error.InvalidRequestError:
+                logging.warning("OpenAI API Invalid Request: Prompt was filtered")
+                return {
+                    "choices": [
+                        {"message": {"content": "Invalid Request: Prompt was filtered"}}
+                    ]
+                }
+            except openai.error.APIConnectionError:
+                logging.warning(
+                    "OpenAI API Connection Error: Error Communicating with OpenAI"
+                )
+                await asyncio.sleep(10)
+            except openai.error.Timeout:
+                logging.warning("OpenAI APITimeout Error: OpenAI Timeout")
+                await asyncio.sleep(10)
+            except openai.error.ServiceUnavailableError as e:
+                logging.warning(f"OpenAI service unavailable error: {e}")
+                await asyncio.sleep(10)
             except openai.error.APIError as e:
                 logging.warning(f"OpenAI API error: {e}")
-                break
+                await asyncio.sleep(10)
         return {"choices": [{"message": {"content": ""}}]}
 
 
@@ -137,9 +158,12 @@ async def _throttled_openai_chat_completion_acreate(
             except openai.error.Timeout:
                 logging.warning("OpenAI APITimeout Error: OpenAI Timeout")
                 await asyncio.sleep(10)
+            except openai.error.ServiceUnavailableError as e:
+                logging.warning(f"OpenAI service unavailable error: {e}")
+                await asyncio.sleep(10)
             except openai.error.APIError as e:
                 logging.warning(f"OpenAI API error: {e}")
-                break
+                await asyncio.sleep(10)
         return {"choices": [{"message": {"content": ""}}]}
 
 
